2f7b1be0177c52170d3262ab081b52d7fd9e015e
csPython <cspython@AD25TF04NGF1J-42.local>
2019-03-26 11:49:14 -0400

    Added green hill

--- fireflies.py
+++ fireflies.py
@@ -3,3 +3,8 @@ from graphics import *
 window = GraphWin("Starry Night", 1000, 800)
 window.setCoords(0, 0, 1000, 800)
 window.setBackground(color_rgb(29, 0, 51))
+
+
+hill = Oval(Point(250, 250), Point(800,-600))
+hill.setFill(color_rgb(0,55,0))
+hill.draw(window)
