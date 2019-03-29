feb882f5cabe64a65d2e60388fa434da36fb2adb
csPython <cspython@AD25TF04NGF1J-42.local>
2019-03-29 09:16:56 -0400

    Revamped hill

--- fireflies.py
+++ fireflies.py
@@ -11,15 +11,12 @@ window = GraphWin("Starry Night", 1000, 800)
 window.setCoords(0, 0, 1000, 800)
 window.setBackground(color_rgb(29, 0, 51))
 
-<<<<<<< HEAD
-=======
 grass = Rectangle(Point(0, 0), Point(1000, 200))
 grass.setFill(color_rgb(0, 55, 0))
 grass.setOutline(color_rgb(0, 55, 0))
 grass.draw(window)
 
->>>>>>> hill
-hill = Oval(Point(250, 250), Point(800,-600))
+hill = Oval(Point(100, 330), Point(900,-600))
 hill.setFill(color_rgb(0,55,0))
 hill.setOutline(color_rgb(0, 55, 0))
 hill.draw(window)
