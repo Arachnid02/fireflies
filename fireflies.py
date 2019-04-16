7c9997527760b78cb2eefad7f1961c2bb8a9228c
csPython <cspython@AD25TF04NGF1J-42.local>
2019-04-16 11:04:36 -0400

    first flower

--- fireflies.py
+++ fireflies.py
@@ -15,9 +15,15 @@ def draw_stars(X, Y, rad, col, window):
 
 def draw_sunflowers(X, Y, rad, col1, col2, window):
     flower1Core = draw_circle(X, Y, rad, col1, window)
-    for i in range(5):
-        flower1Pedal = draw_circle(X - rad, Y - rad, rad, col2, window)
-
+    for i in range(7):
+        flower1Pedal = draw_circle(X - rad / 1.5, Y - rad, rad / 2.3, col2, window) #bottom left
+        flower2Pedal = draw_circle(X + rad, Y + rad, rad / 2.3, col2, window) #upper right
+        flower3Pedal = draw_circle(X + rad / 1.5, Y - rad, rad / 2.3, col2, window) # bottom right
+        flower4Pedal = draw_circle(X - rad, Y + rad, rad / 2.3, col2, window) #upper left
+        flower5Pedal = draw_circle(X + rad / 15, Y + rad, rad/ 2.3, col2, window) #top
+        flower6Pedal = draw_circle(X + rad, Y + rad / 15, rad / 2.3, col2, window) #left
+        flower7Pedal = draw_circle(X - rad, Y + rad / 15, rad / 2.3, col2, window) #right
+        
 window = GraphWin("Starry Night", 1000, 800)
 window.setCoords(0, 0, 1000, 800)
 window.setBackground(color_rgb(29, 0, 51))
@@ -50,9 +56,9 @@ stem.draw(window)
 
 sunflowerX = 50
 sunflowerY = 200
-sunflowerRadius = 8
+sunflowerRadius = 10
 sunflowerColor1 = (color_rgb(43, 29, 14))
-sunflowerColor2 = "yellow"
+sunflowerColor2 = "orange"
 draw_sunflowers(sunflowerX, sunflowerY, sunflowerRadius, sunflowerColor1,
                 sunflowerColor2, window)
 
